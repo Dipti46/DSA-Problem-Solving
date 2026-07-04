238f2ded1218cecc2a8133ede486fe5aed333874
Dipti46 <diptioraon46@gmail.com>
2026-07-04 19:39:04 +0530

    Time: 3 ms (75.35%), Space: 20.3 MB (17.55%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int v = numCourses;
+        vector<vector<int>> adj(v);
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> indegree(v,0);
+
+        for(int i=0; i<v; i++){
+            for(auto neigh : adj[i])
+            indegree[neigh]++;
+        }
+
+        queue<int> q;
+
+        for(int i=0; i<v; i++){
+            if(indegree[i]==0)
+            q.push(i);
+        }
+
+        vector<int> topo;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            topo.push_back(node);
+
+            for(auto neigh : adj[node]){
+                indegree[neigh]--;
+
+                if(indegree[neigh]==0)
+                q.push(neigh);
+            }
+        }
+
+        if(topo.size()==v) return true;
+        return false;
+    }
+};
